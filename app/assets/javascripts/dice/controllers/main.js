7cd83328e305208ca1a5b03f3433f8fd1f017eac
Sam Perdue <firexion@gmail.com>
2014-05-24 23:12:32 -0400

    Custom roll now handles multiplication

--- app/assets/javascripts/dice/controllers/main.js
+++ app/assets/javascripts/dice/controllers/main.js
@@ -1,12 +1,12 @@
 'use strict';
-  
+
 angular.module('diceAngularApp')
   .controller('DiceController', function ($scope) {
-    
+
     $scope.onlyNumbers = /^\d+$/;
-    
+
     $scope.dice = diceBySystem[0];
-    
+
     $scope.roll = {
       "multiplier": 1,
       'numRolls': 1,
@@ -14,11 +14,11 @@ angular.module('diceAngularApp')
       'beforeMultiplyBonus': 0,
       'afterMultiplyBonus': 0
     };
-    
+
     $scope.rollHistory = "Welcome to my multi-system dice roller!\n";
-    
+
     $scope.result = 0;
-    
+
     $scope.quickRoll = function (die, $event) {
       $scope.roll = {
         "multiplier": 1,
@@ -27,62 +27,64 @@ angular.module('diceAngularApp')
         'beforeMultiplyBonus': 0,
         'afterMultiplyBonus': 0
       };
-      
+
       $scope.rollDice( $event );
-    
+
     };
-    
+
     $scope.rollDice = function ( $event ) {
       var total = 0;
       var rolls = new Array( $scope.roll.numRolls );
-      
+
       var beforeMultiplyBonus = parseInt($scope.roll.beforeMultiplyBonus) || 0;
       var afterMultiplyBonus = parseInt($scope.roll.afterMultiplyBonus) || 0;
-      
-      var rollString = "Rolling " + getRollDescription($scope.roll.multiplier, $scope.roll.numRolls, $scope.roll.numSides, beforeMultiplyBonus, afterMultiplyBonus);	
+
+      var rollString = "Rolling " + getRollDescription($scope.roll.multiplier, $scope.roll.numRolls, $scope.roll.numSides, beforeMultiplyBonus, afterMultiplyBonus);
       rollString += "\n";
-      
+
       if ($scope.roll.multiplier > 1) {
         rollString += $scope.roll.multiplier + " * ( ";
       }
-      
+
       rollString += "( "
-      
+
       for( var i = 0; i < $scope.roll.numRolls; i++ ) {
         var rollOne = randomInt( 1, $scope.roll.numSides );
         if( i == 0 )
           rollString += rollOne;
         else
           rollString += " + " + rollOne;
-        
+
         rolls[i] = rollOne;
         total += rollOne;
       }
-      
+
       rollString += " )";
-      
+
       total += beforeMultiplyBonus;
- 
+
       if( beforeMultiplyBonus != 0 )
         rollString += " + " + beforeMultiplyBonus;
-      
+
       if ($scope.roll.multiplier > 1) {
+        total *= $scope.roll.multiplier;
         rollString += " )";
       }
-      
+
       if (afterMultiplyBonus != 0) {
+        total += afterMultiplyBonus;
         rollString += " + " + afterMultiplyBonus;
       }
-      
+
       rollString += " = " + total + "\n";
-      
+
       $scope.rollHistory += rollString;
-      
+
       angular.element($event.target).scope().result = total;
     };
-    
+
     $scope.clearHistory = function() {
       $scope.rollHistory = "History cleared\n";
     };
-    
+
   });
